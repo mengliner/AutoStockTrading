d296f07df2ee7f964dd1277942b12943b6c8e32b
mengliner <1219948661@qq.com>
2025-12-16 09:02:34 +0800

    初版迭代

--- api/stock_router.py
+++ api/stock_router.py
@@ -1,10 +1,18 @@
+'''
+Author: mengliner 1219948661@qq.com
+Date: 2025-12-15 16:22:47
+LastEditors: mengliner 1219948661@qq.com
+LastEditTime: 2025-12-16 00:48:13
+FilePath: \AutoStockTrading\api\stock_router.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 from fastapi import APIRouter, Depends, HTTPException
 from db.mysql_client import MySQLClient
 from api.auth import get_current_user
 
 router = APIRouter(prefix="/api/stock")
 
-@router.get("/basic")
+@router.get("/basic",summary="获取股票基础信息列表")
 def get_stock_basic(page: int = 1, page_size: int = 20, user=Depends(get_current_user)):
     with MySQLClient() as db:
         return db.query_paginate("SELECT * FROM stock_basic", page, page_size)
